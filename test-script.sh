f7dc3351130634d2d77fa364b0abb7152d35234f
Sparsh <sparsh.bhardwaj@knoldus.com>
2021-12-16 10:26:12 +0530

    concourse try 15

--- test-script.sh
+++ test-script.sh
@@ -1,4 +1,4 @@
-PATH=$(npm bin):$PATH
+#! /bin/bash
 
 alias npm-exec='PATH=$(npm bin):$PATH'
 
@@ -9,3 +9,5 @@ echo "will start the execution now"
 # cd node_modules/.bin/cypress
 # 
 # node_modules/.bin/cypress run
+
+pwd 
