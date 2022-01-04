7ac86269ac984b9001d6d8c9d94d3c9f3b94233b
Sparsh <sparsh.bhardwaj@knoldus.com>
2022-01-04 09:55:59 +0530

    26

--- ci/e2e.sh
+++ ci/e2e.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
-npm bin
+
 cd cypressgit/
 
-npm install cypress --save-dev
-
-
-# cd /root/.cache/Cypress/7.7.0
-# npm run testHeadless
-# npx cypress run
-
-./node_modules/.bin/cypress run
-
-/tmp/build/3c49d9cc/node_modules/.bin/cypress run
 $(npm bin)/cypress run
